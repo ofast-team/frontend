f863b2476a10b33f6aff8e7c5f153931eef8b2cc
Cameron Custer <ccuster@philadelphia.ptl.ucf.edu>
2023-10-17 10:22:34 -0400

    create the about-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React from 'react'
+import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Raleway, sans-serif',
+    fontSize: 16,
+    fontWeightRegular: 400,
+    fontWeightBold: 700,
+    h3: {
+      fontSize: '3rem',
+      fontWeight: 'bold',
+    },
+    body1: {
+      fontSize: '1.25rem',
+      lineHeight: 1.5,
+    },
+  },
+  palette: {
+    primary: {
+      main: '#04364A',
+    },
+    secondary: {
+      main: '#DAFFFB',
+    },
+  },
+})
 
 export function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div>
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Container, Typography, Avatar, Box } from '@mui/material'
+
+interface Profile {
+  name: string
+  role: string
+  bio: string
+  photoUrl: string
+}
+
+// TODO(cam): add a contact info page for people to reach out to us and give feedback
+// need a professional email for this
+export default function AboutPage() {
+  const aboutText: string = `
+    O(fast) is created by students for students. We want to help you learn!
+    We believe in learning by doing which is why we created a set of
+    interactive tutorials to help students learn about data structures and
+    algorithms for competitive programming applications.
+    `
+
+  // Sample data for profile information
+  const profiles: Profile[] = [
+    {
+      name: 'Andrew Tate',
+      role: 'Alpha Male',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.',
+      photoUrl:
+        'https://gibbonsgazette.org/wp-content/uploads/2022/10/Who-is-Andrew-Tate-e1667939153903-900x680.jpg',
+    },
+    {
+      name: 'Tristan Tate',
+      role: 'Sigma Male',
+      bio: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+      photoUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR5r4MZXjXeKJgBTe66EDtTyDgiTeg2Oqo1pGl5Tpfv5YgxF3mleIEVjW4HZzQXLwhvcY&usqp=CAU',
+    },
+  ]
+
+  return (
+    <Container>
+      <Typography variant="h3" gutterBottom color="primary">
+        About
+      </Typography>
+      <Typography variant="body1" paragraph>
+        {aboutText}
+      </Typography>
+      <Typography variant="h3" gutterBottom color="primary">
+        Team
+      </Typography>
+      {profiles.map((profile, index) => (
+        <Box key={index} mb={3} display="flex" alignItems="center">
+          <Avatar
+            alt={`${profile.name} Photo`}
+            src={profile.photoUrl}
+            sx={{ width: 100, height: 100, marginRight: 2 }}
+          />
+          <div>
+            <Typography variant="h5" component="div">
+              {profile.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              sx={{
+                fontStyle: 'italic',
+              }}
+            >
+              {profile.role}
+            </Typography>
+            <Typography variant="body2">{profile.bio}</Typography>
+          </div>
+        </Box>
+      ))}
+    </Container>
+  )
+}
