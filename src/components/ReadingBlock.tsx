e11d360acfa9c0fbda00c7980cacd881d2304e4a
Asir Alam <alvi.abrar29@gmail.com>
2023-11-02 17:22:17 -0400

    Clean up, add empty MCQ and FITB block components

--- src/components/ReadingBlock.tsx
+++ src/components/ReadingBlock.tsx
@@ -2,36 +2,26 @@ import React from 'react'
 import Markdown from 'react-markdown'
 import rehypeMathjax from 'rehype-mathjax/svg'
 import remarkMath from 'remark-math'
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-
-import { MathJax, MathJaxContext } from 'better-react-mathjax'
-
-import {
-  Button,
-  Typography
-} from '@mui/material'
+import { Typography } from '@mui/material'
 
 interface ReadingBlockProps {
   content: string;
 }
 
 export default function ReadingBlock(props: ReadingBlockProps) {
-  
+
   return (
     <Typography gutterBottom color="primary" component={'span'}>
-      {/* <MathJaxContext>
-        <h2>Basic MathJax example with Latex</h2>
-        <MathJax>{"\\(\\frac{10}{4x} \\approx 2^{12}\\)"}</MathJax>
-      </MathJaxContext> */}
       <Markdown
         children={props.content}
-        remarkPlugins={[remarkMath]} 
+        remarkPlugins={[remarkMath]}
         rehypePlugins={[rehypeMathjax]}
         components={{
           code(props) {
-            const {children, className, node, ...rest} = props
+            const { children, className, node, ...rest } = props
             const match = /language-(\w+)/.exec(className || '')
             return match ? (
               <SyntaxHighlighter
@@ -50,6 +40,6 @@ export default function ReadingBlock(props: ReadingBlockProps) {
         }}
       />
     </Typography>
-      
+
   )
 }
