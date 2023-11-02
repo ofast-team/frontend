cb2f1b25f06971107378519ed404c78e6afb3200
Cameron Custer <ccuster@philadelphia.ptl.ucf.edu>
2023-11-02 15:05:42 -0400

    test formatting action

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
+
+
+
+
+
+
+
   <React.StrictMode>
     <BrowserRouter>
       <App />
