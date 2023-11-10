645804f695402ad5684fba81fe8c5320fed453ed
Asir Alam <alvi.abrar29@gmail.com>
2023-11-10 13:22:04 -0500

    added lesson URLs + dynamic learn page rendering

--- src/pages/LearnPage.tsx
+++ src/pages/LearnPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 import {
   Box,
   Container,
@@ -13,11 +12,7 @@ import { styled } from '@mui/material/styles'
 
 import SearchBar from '../components/SearchBar'
 import InlineSpacing from '../components/InlineSpacing'
-import ReadingBlock from '../components/ReadingBlock'
-import LessonPage from './LessonPage'
-import MCQBlock from '../components/MCQBlock'
-import FITBBlock from '../components/FITBBlock'
-import fetchLessonContent from '../functions/FetchLessonContent'
+import { Link } from 'react-router-dom'
 
 const LessonButton = styled(Button)({
   border: '1px solid',
@@ -64,25 +59,59 @@ function LessonGroup({ title, children }: LessonGroupProps) {
   )
 }
 
-const dp = await fetchLessonContent('dynamic_programming/lesson.md')
-const markdown1 = await fetchLessonContent('dynamic_programming/markdown1.md')
-const markdown2 = await fetchLessonContent('dynamic_programming/markdown2.md')
-const markdown3 = await fetchLessonContent('dynamic_programming/markdown3.md')
+interface lesson {
+  group: string
+  name: string
+  urlParam: string
+}
 
-const q1 =
-  'Which of the problems below can be solved effectively using dynamic programming.'
-const ans1 = ['Coin denomination', 'Knapsack Problem', 'Sorting List']
-const correct1 = ['Coin denomination', 'Knapsack Problem']
-
-const q2 =
-  'In dynamic programming, what is the key characteristic that distinguishes it from other algorithmic approaches?'
-const ans2 = ['Recursion', 'Iteration', 'Memoization']
-const correct2 = ['Memoization']
+const lessons: lesson[] = [
+  {
+    group: 'Intro Algo Design',
+    name: 'Brute Force',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Algo Design',
+    name: 'Intro Greedy',
+    urlParam: 'greedy',
+  },
+  {
+    group: 'Intro Algo Design',
+    name: 'Time & Memory Analysis',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Data Structures',
+    name: 'Prefix Sums',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Data Structures',
+    name: 'Lists & Vectors',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Data Structures',
+    name: 'Stacks & Queues',
+    urlParam: 'dp',
+  },
+]
 
 export default function LearnPage() {
-  const [lesson, setLesson] = useState<React.ReactNode>(null)
+  const groupedLessons: {
+    [key: string]: lesson[]
+  } = {}
 
-  return !lesson ? (
+  lessons.forEach((lesson) => {
+    if (!(lesson.group in groupedLessons)) {
+      groupedLessons[lesson.group] = []
+    }
+
+    groupedLessons[lesson.group].push(lesson)
+  })
+
+  return (
     <Container sx={{ p: 15 }}>
       <Typography variant="h3" gutterBottom color="primary">
         Learn
@@ -90,75 +119,34 @@ export default function LearnPage() {
 
       <SearchBar />
 
-      <LessonGroup title="Intro Algo Design">
-        <InlineSpacing spacing={40} />
+      {Object.entries(groupedLessons).map((lessonGroup, groupIndex) => {
+        return (
+          <>
+            <LessonGroup title={lessonGroup[0].toString()}>
+              <InlineSpacing spacing={40} />
+              {lessonGroup[1].map((lesson) => (
+                <>
+                  <LessonButton>
+                    <Link
+                      style={{ color: 'inherit', textDecoration: 'none' }}
+                      to={'/learn/' + lesson.urlParam}
+                    >
+                      {lesson.name}
+                    </Link>
+                  </LessonButton>
+                  <InlineSpacing spacing={60} />
+                </>
+              ))}
 
-        <LessonButton
-          onClick={() =>
-            setLesson(
-              <LessonPage
-                blocks={[
-                  <ReadingBlock content={dp} />,
-                  <MCQBlock
-                    question={q1}
-                    answerOptions={ans1}
-                    correctOptions={correct1}
-                  />,
-                  <MCQBlock
-                    question={q2}
-                    answerOptions={ans2}
-                    correctOptions={correct2}
-                  />,
-                  <FITBBlock
-                    question={
-                      'Is this the real life? Is this just {fantasy}? ' +
-                      'Caught in a {landslide}, no escape from reality. ' +
-                      'Open your eyes, look up to the {skies} and see...'
-                    }
-                  />,
-                  <ReadingBlock content={markdown1} />,
-                  <ReadingBlock content={markdown2} />,
-                  <ReadingBlock content={markdown3} />,
-                ]}
-              />,
-            )
-          }
-        >
-          Brute Force
-        </LessonButton>
+              <NextButton>
+                <ArrowForwardIcon style={{ fontSize: '3rem' }} />
+              </NextButton>
+            </LessonGroup>
 
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Intro Greedy</LessonButton>
-
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Time & Memory Analysis</LessonButton>
-      </LessonGroup>
-
-      <Divider />
-
-      <LessonGroup title="Intro Data Structures">
-        <InlineSpacing spacing={40} />
-
-        <LessonButton>Prefix Sums</LessonButton>
-
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Lists & Vectors</LessonButton>
-
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Stacks & Queues</LessonButton>
-
-        <InlineSpacing spacing={40} />
-
-        <NextButton>
-          <ArrowForwardIcon style={{ fontSize: '3rem' }} />
-        </NextButton>
-      </LessonGroup>
+            {groupIndex + 1 < Object.entries(groupedLessons).length && <Divider />}
+          </>
+        )
+      })}
     </Container>
-  ) : (
-    lesson
   )
 }
