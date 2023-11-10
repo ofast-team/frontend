b134100c997d900540a93a463563c6cd8e12a60f
Asir Alam <alvi.abrar29@gmail.com>
2023-11-10 13:22:04 -0500

    added lesson URLs + dynamic learn page rendering

--- src/pages/LearnPage.tsx
+++ src/pages/LearnPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 import {
   Box,
   Container,
@@ -13,10 +12,7 @@ import { styled } from '@mui/material/styles'
 
 import SearchBar from '../components/SearchBar'
 import InlineSpacing from '../components/InlineSpacing'
-import ReadingBlock from '../components/ReadingBlock'
-import LessonPage from './LessonPage'
-import MCQBlock from '../components/MCQBlock'
-import FITBBlock from '../components/FITBBlock'
+import { Link } from 'react-router-dom'
 
 const LessonButton = styled(Button)({
   border: '1px solid',
@@ -63,89 +59,59 @@ function LessonGroup({ title, children }: LessonGroupProps) {
   )
 }
 
-const dp = `
-# Dynamic Programming
-Dynamic programming is a powerful algorithmic technique used to solve complex problems by breaking them down into **simpler subproblems** and efficiently solving each subproblem **only once**. It is particularly useful for optimization problems and problems with overlapping substructures, where the same subproblems are encountered multiple times.
+interface lesson {
+  group: string
+  name: string
+  urlParam: string
+}
 
-At the core of dynamic programming is the idea of storing and reusing solutions to subproblems, which leads to significant time and computational savings. This technique is often used in problems involving sequences, such as finding the shortest path in a graph, calculating Fibonacci numbers, or solving the knapsack problem.
-
-One of the key advantages of dynamic programming is its ability to transform a problem into a more manageable form by building a solution iteratively. This process of solving subproblems and combining their solutions to tackle larger, more complex problems enables dynamic programming to provide efficient solutions to a wide range of computational challenges.
-
-## Memoization
-**Memoization** is a critical aspect of dynamic programming that involves storing the results of subproblem solutions to avoid redundant calculations. By memorizing these results in a table or data structure, dynamic programming ensures that each subproblem is **solved only once** and its solution is retrieved when needed, significantly improving the algorithm’s efficiency. This technique not only **reduces time complexity** but also simplifies the code and enhances its readability, making dynamic programming an invaluable tool in solving a diverse array of computational problems.
-
-~~~python
-# Create a dictionary to store previously computed Fibonacci values
-memo = {}
-
-def fibonacci(n):
-    # Check if the value is already memoized
-    if n in memo:
-        return memo[n]
-
-    # Base cases
-    if n <= 1:
-        return n
-
-    # Calculate and memoize the Fibonacci value
-    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
-    return memo[n]
-
-# Example usage:
-n = 10
-result = fibonacci(n)
-print(f"The {n}-th Fibonacci number is {result}")
-~~~
-`
-
-const markdown1 = `
-# Reading 1
-This is how to compute the $i$th fibonacci number: \n
-$F_i = F_{i-1} + F_{i-2}$ \n
-$\\sum_{n=1}^{\\infty} 2^{-n} = 1$ \n
-$\\pi \\approx 3.14159$ \n
-$\\pm \\, 0.2$ \n
-$\\dfrac{0}{1} \\neq \\infty$ \n
-`
-
-const markdown2 = `
-# Reading 2
-$0 < x < 1$ \n
-$0 \\leq x \\leq 1$ \n
-$x \\geq 10$ \n
-$\\forall \\, x \\in (1,2)$ \n
-$\\exists \\, x \\notin [0,1]$ \n
-$A \\subset B$ \n
-$A \\subseteq B$ \n 
-$A \\cup B$ \n
-$A \\cap B$ \n
-`
-
-const markdown3 = `
-# Reading 3
-$X \\implies Y$ \n
-$X \\impliedby Y$ \n
-$a \\to b$ \n
-$a \\longrightarrow b$ \n
-$a \\Rightarrow b$ \n
-$a \\Longrightarrow b$ \n
-$a \\propto b$
-`
-
-const q1 =
-  'Which of the problems below can be solved effectively using dynamic programming.'
-const ans1 = ['Coin denomination', 'Knapsack Problem', 'Sorting List']
-const correct1 = ['Coin denomination', 'Knapsack Problem']
-
-const q2 =
-  'In dynamic programming, what is the key characteristic that distinguishes it from other algorithmic approaches?'
-const ans2 = ['Recursion', 'Iteration', 'Memoization']
-const correct2 = ['Memoization']
+const lessons: lesson[] = [
+  {
+    group: 'Intro Algo Design',
+    name: 'Brute Force',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Algo Design',
+    name: 'Intro Greedy',
+    urlParam: 'greedy',
+  },
+  {
+    group: 'Intro Algo Design',
+    name: 'Time & Memory Analysis',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Data Structures',
+    name: 'Prefix Sums',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Data Structures',
+    name: 'Lists & Vectors',
+    urlParam: 'dp',
+  },
+  {
+    group: 'Intro Data Structures',
+    name: 'Stacks & Queues',
+    urlParam: 'dp',
+  },
+]
 
 export default function LearnPage() {
-  const [lesson, setLesson] = useState<React.ReactNode>(null)
+  const groupedLessons: {
+    [key: string]: lesson[]
+  } = {}
 
-  return !lesson ? (
+  lessons.forEach((lesson) => {
+    if (!(lesson.group in groupedLessons)) {
+      groupedLessons[lesson.group] = []
+    }
+
+    groupedLessons[lesson.group].push(lesson)
+  })
+
+  return (
     <Container sx={{ p: 15 }}>
       <Typography variant="h3" gutterBottom color="primary">
         Learn
@@ -153,75 +119,34 @@ export default function LearnPage() {
 
       <SearchBar />
 
-      <LessonGroup title="Intro Algo Design">
-        <InlineSpacing spacing={40} />
+      {Object.entries(groupedLessons).map((lessonGroup, groupIndex) => {
+        return (
+          <>
+            <LessonGroup title={lessonGroup[0].toString()}>
+              <InlineSpacing spacing={40} />
+              {lessonGroup[1].map((lesson) => (
+                <>
+                  <LessonButton>
+                    <Link
+                      style={{ color: 'inherit', textDecoration: 'none' }}
+                      to={'/learn/' + lesson.urlParam}
+                    >
+                      {lesson.name}
+                    </Link>
+                  </LessonButton>
+                  <InlineSpacing spacing={60} />
+                </>
+              ))}
 
-        <LessonButton
-          onClick={() =>
-            setLesson(
-              <LessonPage
-                blocks={[
-                  <ReadingBlock content={dp} />,
-                  <MCQBlock
-                    question={q1}
-                    answerOptions={ans1}
-                    correctOptions={correct1}
-                  />,
-                  <MCQBlock
-                    question={q2}
-                    answerOptions={ans2}
-                    correctOptions={correct2}
-                  />,
-                  <FITBBlock
-                    question={
-                      'Is this the real life? Is this just {fantasy}? ' +
-                      'Caught in a {landslide}, no escape from reality. ' +
-                      'Open your eyes, look up to the {skies} and see...'
-                    }
-                  />,
-                  <ReadingBlock content={markdown1} />,
-                  <ReadingBlock content={markdown2} />,
-                  <ReadingBlock content={markdown3} />,
-                ]}
-              />,
-            )
-          }
-        >
-          Brute Force
-        </LessonButton>
+              <NextButton>
+                <ArrowForwardIcon style={{ fontSize: '3rem' }} />
+              </NextButton>
+            </LessonGroup>
 
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Intro Greedy</LessonButton>
-
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Time & Memory Analysis</LessonButton>
-      </LessonGroup>
-
-      <Divider />
-
-      <LessonGroup title="Intro Data Structures">
-        <InlineSpacing spacing={40} />
-
-        <LessonButton>Prefix Sums</LessonButton>
-
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Lists & Vectors</LessonButton>
-
-        <InlineSpacing spacing={60} />
-
-        <LessonButton>Stacks & Queues</LessonButton>
-
-        <InlineSpacing spacing={40} />
-
-        <NextButton>
-          <ArrowForwardIcon style={{ fontSize: '3rem' }} />
-        </NextButton>
-      </LessonGroup>
+            {groupIndex + 1 < Object.entries(groupedLessons).length && <Divider />}
+          </>
+        )
+      })}
     </Container>
-  ) : (
-    lesson
   )
 }
--- src/pages/LessonPage.tsx
+++ src/pages/LessonPage.tsx
@@ -3,9 +3,11 @@ import { Box, Container } from '@mui/material'
 
 import './LessonPage.css'
 
-interface LessonPageProps {
-  blocks: ReadonlyArray<React.ReactNode>
-}
+import ReadingBlock from '../components/ReadingBlock'
+import { useParams, Navigate } from 'react-router-dom'
+
+import MCQBlock from '../components/MCQBlock'
+import FITBBlock from '../components/FITBBlock'
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window
@@ -74,7 +76,117 @@ function LessonBlockWrapper({
   )
 }
 
-export default function LessonPage({ blocks }: LessonPageProps) {
+const dp = `
+# Dynamic Programming
+Dynamic programming is a powerful algorithmic technique used to solve complex problems by breaking them down into **simpler subproblems** and efficiently solving each subproblem **only once**. It is particularly useful for optimization problems and problems with overlapping substructures, where the same subproblems are encountered multiple times.
+
+At the core of dynamic programming is the idea of storing and reusing solutions to subproblems, which leads to significant time and computational savings. This technique is often used in problems involving sequences, such as finding the shortest path in a graph, calculating Fibonacci numbers, or solving the knapsack problem.
+
+One of the key advantages of dynamic programming is its ability to transform a problem into a more manageable form by building a solution iteratively. This process of solving subproblems and combining their solutions to tackle larger, more complex problems enables dynamic programming to provide efficient solutions to a wide range of computational challenges.
+
+## Memoization
+**Memoization** is a critical aspect of dynamic programming that involves storing the results of subproblem solutions to avoid redundant calculations. By memorizing these results in a table or data structure, dynamic programming ensures that each subproblem is **solved only once** and its solution is retrieved when needed, significantly improving the algorithm’s efficiency. This technique not only **reduces time complexity** but also simplifies the code and enhances its readability, making dynamic programming an invaluable tool in solving a diverse array of computational problems.
+
+~~~python
+# Create a dictionary to store previously computed Fibonacci values
+memo = {}
+
+def fibonacci(n):
+    # Check if the value is already memoized
+    if n in memo:
+        return memo[n]
+
+    # Base cases
+    if n <= 1:
+        return n
+
+    # Calculate and memoize the Fibonacci value
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return memo[n]
+
+# Example usage:
+n = 10
+result = fibonacci(n)
+print(f"The {n}-th Fibonacci number is {result}")
+~~~
+`
+
+const markdown1 = `
+# Reading 1
+This is how to compute the $i$th fibonacci number: \n
+$F_i = F_{i-1} + F_{i-2}$ \n
+$\\sum_{n=1}^{\\infty} 2^{-n} = 1$ \n
+$\\pi \\approx 3.14159$ \n
+$\\pm \\, 0.2$ \n
+$\\dfrac{0}{1} \\neq \\infty$ \n
+`
+
+const markdown2 = `
+# Reading 2
+$0 < x < 1$ \n
+$0 \\leq x \\leq 1$ \n
+$x \\geq 10$ \n
+$\\forall \\, x \\in (1,2)$ \n
+$\\exists \\, x \\notin [0,1]$ \n
+$A \\subset B$ \n
+$A \\subseteq B$ \n 
+$A \\cup B$ \n
+$A \\cap B$ \n
+`
+
+const markdown3 = `
+# Reading 3
+$X \\implies Y$ \n
+$X \\impliedby Y$ \n
+$a \\to b$ \n
+$a \\longrightarrow b$ \n
+$a \\Rightarrow b$ \n
+$a \\Longrightarrow b$ \n
+$a \\propto b$
+`
+
+const q1 =
+  'Which of the problems below can be solved effectively using dynamic programming.'
+const ans1 = ['Coin denomination', 'Knapsack Problem', 'Sorting List']
+const correct1 = ['Coin denomination', 'Knapsack Problem']
+
+const q2 =
+  'In dynamic programming, what is the key characteristic that distinguishes it from other algorithmic approaches?'
+const ans2 = ['Recursion', 'Iteration', 'Memoization']
+const correct2 = ['Memoization']
+
+const lessons = {
+  dp: [
+    <ReadingBlock content={dp} />,
+    <MCQBlock question={q1} answerOptions={ans1} correctOptions={correct1} />,
+    <MCQBlock question={q2} answerOptions={ans2} correctOptions={correct2} />,
+    <FITBBlock
+      question={
+        'Is this the real life? Is this just {fantasy}? ' +
+        'Caught in a {landslide}, no escape from reality. ' +
+        'Open your eyes, look up to the {skies} and see...'
+      }
+    />,
+    <ReadingBlock content={markdown1} />,
+    <ReadingBlock content={markdown2} />,
+    <ReadingBlock content={markdown3} />,
+  ],
+  greedy: [
+    <ReadingBlock content={`# Welcome to Greedy \n Let's get started`} />,
+    <ReadingBlock content={markdown1} />,
+    <ReadingBlock content={markdown2} />,
+    <ReadingBlock content={markdown3} />,
+  ],
+}
+
+export default function LessonPage() {
+  const params = useParams()
+
+  if (!params.lesson || !(params.lesson in lessons)) {
+    return <Navigate to="/learn" replace={true} />
+  }
+
+  const blocks = lessons[params.lesson]
   const blockRefs = useRef(new Array(blocks.length))
   const [offsetY, setOffsetY] = useState(0)
   const [windowDimensions, setWindowDimensions] = useState(
