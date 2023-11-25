c693fe8bb295e56e34f6bfca45d37a5e12b38796
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-11-25 16:00:23 -0500

    Added Redux-Persist, user stays log'd in on refrsh

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const reducers = combineReducers({
+  user: userReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 })
 
 export type RootState = ReturnType<typeof store.getState>
