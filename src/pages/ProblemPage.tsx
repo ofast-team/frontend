8904712a46fa6f329be47f7b97ba2220b01d8498
Asir Alam <alvi.abrar29@gmail.com>
2024-01-09 23:47:48 -0500

    Basic Problem Page Layout

--- src/pages/ProblemPage.tsx
+++ src/pages/ProblemPage.tsx
@@ -1,11 +1,59 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { Box, Typography } from '@mui/material'
+import Markdown from 'react-markdown'
+import rehypeMathjax from 'rehype-mathjax/svg'
+import remarkMath from 'remark-math'
+
+const problems = {
+  twosum: {
+    title: 'Two Sum',
+    text: 'Alice and Bob want to add two numbers. Can you help them?',
+    problem:
+      'Given two integers, a and b, write a program that outputs their sum.',
+    input: '3 5',
+    output: '8',
+  },
+}
 
 export default function ProblemPage() {
   const params = useParams()
-  console.log(params.problem)
+  const problemID: string = params.problem as string
+
+  if (!(problemID in problems)) {
+    return <Box pt={15}>Problem not found</Box>
+  }
+
+  const problem = problems[problemID]
 
   return (
-    <div>ProblemPage</div>
+    <Box pt={15}>
+      <Typography
+        className="markdown"
+        gutterBottom
+        color="primary"
+        component={'span'}
+      >
+        <h1>{problem.title}</h1>
+        <Markdown
+          children={problem.text}
+          remarkPlugins={[remarkMath]}
+          rehypePlugins={[rehypeMathjax]}
+        />
+
+        <h2>Problem</h2>
+        <Markdown
+          children={problem.problem}
+          remarkPlugins={[remarkMath]}
+          rehypePlugins={[rehypeMathjax]}
+        />
+        
+        <h2>Input</h2>
+        {problem.input}
+        
+        <h2>Output</h2>
+        {problem.output}
+      </Typography>
+    </Box>
   )
 }
