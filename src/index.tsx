c69971721c48750ad596eeadf5aff94ead42a929
Cameron Custer <ccuster@philadelphia.ptl.ucf.edu>
2024-01-11 17:09:10 -0500

    rectify merge conflicts and allow successful rebase

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,20 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Persistor, persistStore } from 'redux-persist'
+
+const persistor : Persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor= {persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
