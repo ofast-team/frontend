184b3511e651d16400735cdc0ca681efcfd156c0
Asir Alam <alvi.abrar29@gmail.com>
2024-01-11 16:37:41 -0500

    react-syntax-highlighter

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -2,6 +2,27 @@ import React from 'react'
 import Example from './Example.mdx'
 import '../components/ReadingBlock.css'
 import { Typography } from '@mui/material'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+function code(props) {
+  const { children, className, ...rest } = props
+  const match = /language-(\w+)/.exec(className || '')
+  return match ? (
+    <SyntaxHighlighter
+      {...rest}
+      children={String(children).replace(/\n$/, '')}
+      style={oneDark}
+      language={match[1]}
+      PreTag="div"
+      showLineNumbers
+    />
+  ) : (
+    <code {...rest} className={className}>
+      {children}
+    </code>
+  )
+}
 
 export default function TestPage() {
   return (
@@ -11,7 +32,7 @@ export default function TestPage() {
       color="primary"
       component={'span'}
     >
-      <Example/>
+      <Example components={{ code }} />
     </Typography>
   )
 }
