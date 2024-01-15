8b9cff3329b17df4fb8a8cc2fb745041bd72d337
sd0623 <deegutlasathwika@gmail.com>
2024-01-15 01:41:11 -0500

    files upload

--- src/pages/SubmitPage.tsx
+++ src/pages/SubmitPage.tsx
@@ -1,21 +1,100 @@
-import { Container, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Container, Typography } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import InfoIcon from '@mui/icons-material/InfoOutlined'
+
+declare module 'react' {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+    // extends React's HTMLAttributes
+    directory?: string
+    webkitdirectory?: string
+  }
+}
 
 export default function SubmitPage() {
-  const rules = `1. Please submit code as separate file. Supported languages are C, C++, Java, and Python.
-    2. For test cases, please submit a folder structured with sub-directories for each test case with respective input (.in) and output (.out) files.`
+  // const extensions = ['.c', '.cpp', '.java', '.python']
+  const [codeFile, setCodeFile] = useState<File>()
+  const [testFolder, setTestFolder] = useState<File>()
+
+  useEffect(() => {
+    if (codeFile) {
+      console.log('Uploaded Code File:', codeFile)
+    }
+  }, [codeFile])
+
+  useEffect(() => {
+    if (testFolder) {
+      console.log('Uploaded Test Folder:', testFolder)
+    }
+  }, [testFolder])
+
+  const handleCodeFile = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      setCodeFile(file)
+    }
+  }
+
+  const handleTestFolder = (event) => {
+    const folder = event.target.files[0]
+    if (folder) {
+      setTestFolder(folder)
+    }
+  }
 
   return (
     <Container sx={{ p: 15 }}>
       <Typography variant="h3" gutterBottom color="primary">
         Submit
       </Typography>
-      <Typography variant="h4" color="red" sx={{ fontSize: '1.5rem' }}>
-        Instructions:
+
+      <Typography
+        variant="body1"
+        color="red"
+        paragraph
+        mb={3}
+        sx={{ fontSize: '1.3rem' }}
+      >
+        <strong>Supported Languages: C, C++, Java, Python</strong>
       </Typography>
-      <Typography variant="body1" paragraph style={{ whiteSpace: 'pre-line' }}>
-        {rules}
-      </Typography>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          mb: 3,
+        }}
+      >
+        <Typography mr={2}>Program Code</Typography>
+        <input
+          type="file"
+          accept=".c, .cpp, .java, .py"
+          onChange={handleCodeFile}
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <Typography mr={2}>Test Cases Folder</Typography>
+        <input
+          type="file"
+          directory="true"
+          webkitdirectory="true"
+          onChange={handleTestFolder}
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <InfoIcon sx={{ color: 'red', fontSize: '1.3rem', mr: 1 }}></InfoIcon>
+        <Typography variant="subtitle2" color="red">
+          Folder structure: Each test case folder with single input{' '}
+          <strong>(.in)</strong> and output <strong>(.out)</strong> files.
+        </Typography>
+      </Box>
     </Container>
   )
 }
+
+/*
+Time limit text field
+Checkers: diff - check ignore trailing whitespace/blank lines, epsilon - abs & relative fields, token - check case sensitive
+ */
