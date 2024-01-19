c1213b7eb4f518a535edd338e2f672b42dbe9a59
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2024-01-19 16:54:07 -0500

    Latex Expressions Inline

--- src/components/FITBBlock.tsx
+++ src/components/FITBBlock.tsx
@@ -3,6 +3,8 @@ import { ShowAnswerBtn } from './MCQBlock'
 import React, { ReactNode, useState } from 'react'
 import HintDisplay from './HintDisplay'
 import FITB_Blank from './FITB_Blank'
+import FITB_Text from './FITB_Text'
+import MDX from './MDXRenderer'
 
 interface HeaderProps {
   hint: string
@@ -33,26 +35,83 @@ function Header({ hint }: HeaderProps) {
   )
 }
 
-interface FITBBlockProps {
-  hint: string,
-  children: ReactNode,
+function tokenizeText(text: string) : string[] {
+  const tokens : string[] = []
+  let open : boolean = false
+  let curToken : string = ''
+
+  // Split up the MathJax expressions from the plaintext.
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == '$') {
+      if (open) {
+        curToken += text[i]
+        tokens.push(curToken)
+        curToken = ''
+      }
+      else {
+        tokens.push(curToken)
+        curToken = ''
+        curToken += text[i]
+      }
+
+      open = !open
+      
+    }
+    else {
+      curToken += text[i]
+    }
+  }
+
+  if (curToken.length > 0) {
+    tokens.push(curToken)
+  }
+
+  // Take any remaining plaintext and split it up by space.
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i][0] != '$') {
+      const words = tokens[i].split(' ')
+      tokens.splice(i, 1, ...words)
+    }
+  }
+
+  return tokens
 }
 
-export default function FITBBlock({ hint, children }: FITBBlockProps,) {
+interface FITBBlockProps {
+  hint: string
+  children: ReactNode
+}
+
+export default function FITBBlock({ hint, children }: FITBBlockProps) {
   const [submitted, setSubmitted] = useState<boolean>(false)
   const [showAnswer, setShowAnswer] = useState<boolean>(false)
+
+  const renderThese : ReactNode = []
+  React.Children.forEach(children, (child) => {
+    if (child.type === FITB_Blank) {
+      renderThese.push(React.cloneElement(child, {
+        showAnswer: showAnswer,
+        respond: submitted,
+      }))
+    }
+    else if (child?.type === FITB_Text) {
+      const text : string = child?.props.text
+      const tokenizedText : string[] = tokenizeText(text)
+      console.log(tokenizedText)
+      renderThese.push(...tokenizedText.map((x) => <MDX value={x}></MDX>))
+    }
+    else {
+      renderThese.push(child)
+    }
+  })
+
 
   return (
     <Paper sx={{ border: '1px solid #000', my: 2 }}>
       <Header hint={hint} />
       <Box sx={{ p: 3 }}>
-        <Grid container rowGap={2.5} alignItems="center" marginBottom={4}>
-          {React.Children.map(children, (child) => {
-            if (child.type === FITB_Blank) {
-              return React.cloneElement(child, {showAnswer: showAnswer, respond: submitted})
-            }
-            return child
-          })}
+        <Grid container gap = {0.6} alignItems="center" marginBottom={4}>
+          {renderThese}
         </Grid>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
           <ShowAnswerBtn
@@ -68,16 +127,18 @@ export default function FITBBlock({ hint, children }: FITBBlockProps,) {
               onClick={() => {
                 setSubmitted(false)
               }}
-              disabled = {showAnswer}
+              disabled={showAnswer}
             >
               Try Again
             </Button>
           ) : (
             <Button
               variant="contained"
-              onClick={() => {setSubmitted(true)
-              console.log(showAnswer)}}
-              disabled = {showAnswer}
+              onClick={() => {
+                setSubmitted(true)
+                console.log(showAnswer)
+              }}
+              disabled={showAnswer}
             >
               Submit
             </Button>
--- src/components/FITB_Text.tsx
+++ src/components/FITB_Text.tsx
@@ -1,31 +1,13 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import MDX from './MDXRenderer'
-import { Box, Typography } from '@mui/material'
 
 interface FITB_TextProps {
     text: string,
-    children: ReactNode
 }
 
-export default function FITB_Text({children} : FITB_TextProps) {
+export default function FITB_Text({text} : FITB_TextProps) {
 
     return (
-        <Box display={'flex'} flexDirection={'row'} gap={1}>
-            {React.Children.map(children, (child) => {
-                if (typeof child === 'string') {
-                    return (
-                        <Typography>
-                            <MDX value = {child}/>
-                        </Typography>
-                    )
-                }
-                return child
-            })}
-        </Box>
-    )
-    
-
-
-
-        
+        <MDX value = {text}/> 
+        )    
 }
