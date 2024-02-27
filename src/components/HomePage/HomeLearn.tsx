9964325fb09f6c072305deb55a14a54f6df17970
sd0623 <deegutlasathwika@gmail.com>
2024-02-27 00:49:32 -0500

    learn template

--- src/components/HomePage/HomeLearn.tsx
+++ src/components/HomePage/HomeLearn.tsx
@@ -1,58 +1,66 @@
 import React from 'react'
 import { Box, Container, Typography } from '@mui/material'
+import { Settings } from '@mui/icons-material'
 
 interface HomeLearnComponent {
-  title: string
-  text: string
-  svgPath: string
+  svgModule: string
+  svgGraphics: string
 }
 
-function HomeLearnComponent({ title, text, svgPath }: HomeLearnComponent) {
+function HomeLearnComponent({ svgModule, svgGraphics }: HomeLearnComponent) {
   return (
-    <Container>
-      <Typography
-        variant="h3"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flexEnd',
-        }}
-        gutterBottom
-      >
-        Learn
-      </Typography>
-      <Box>
-        <Typography>{title}</Typography>
-        <Typography>{text}</Typography>
-        <img src={svgPath} alt="Learn SVG" />
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'spaceBetween',
+        my: 10,
+      }}
+    >
+      <Box sx={{ width: '50%' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: '10px',
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              justifyContent: 'flex-end',
+              letterSpacing: '5px',
+              textTransform: 'upperCase',
+              alignItems: 'center',
+              color: 'primary',
+            }}
+          >
+            Learn
+          </Typography>
+          <Settings sx={{ fontSize: '5em' }} />
+        </Box>
+        <img src={svgModule} alt="Learn SVG" width="100%" />
+      </Box>
+      <Box sx={{ width: '50%' }}>
+        <img src={svgGraphics} alt="Learn SVG" width="100%" />
       </Box>
     </Container>
   )
 }
 
 export default function HomeLearn() {
+  const objects = [
+    ['assets/learn1_1.svg', 'assets/learn1.svg'],
+    ['assets/learn2_1.svg', 'assets/learn2.svg'],
+    ['assets/learn3_1.svg', 'assets/learn3.svg'],
+  ]
+
   return (
     <Container>
-      <Typography
-        variant="h3"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        gutterBottom
-      >
-        Learn
-      </Typography>
-      <HomeLearnComponent
-        title="Study"
-        text="forever"
-        svgPath="assets/learn1.svg"
-      />
-      <img src="assets/learn1.svg" alt="Learn 1" />
-      <img src="assets/learn2.svg" alt="Learn 2" />
-      <img src="assets/learn3.svg" alt="Learn 3" />
+      {objects.map((index) => (
+        <HomeLearnComponent svgModule={index[0]} svgGraphics={index[1]} />
+      ))}
     </Container>
   )
 }
