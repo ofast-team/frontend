b6bed77fdeb2a418da3ae1c7ddbfe3974baf3a55
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2024-03-15 18:38:01 -0400

    Ran Prettier

--- src/pages/SubmissionsList.tsx
+++ src/pages/SubmissionsList.tsx
@@ -1,7 +1,12 @@
 import { Box, Button, Container, Grid, Typography, styled } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 
-import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom'
 
 import CircleIcon from '@mui/icons-material/Circle'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
@@ -11,7 +16,11 @@ import buildPath from '../path'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store'
 
-import { SubmissionData, emptySubmissionData, formatSubmissionData } from './VerdictPage'
+import {
+  SubmissionData,
+  emptySubmissionData,
+  formatSubmissionData,
+} from './VerdictPage'
 import { useProblemsObject } from '../components/ProblemProvider'
 
 const PendingIcon = styled(CircleIcon)({
@@ -41,14 +50,13 @@ const columnNames = [
 const columnWidths = [2, 2, 2, 1.5, 1.5, 1.5, 1.5]
 
 export default function SubmissionsList() {
-
   const [submissionsTable, setSubmissionsTable] = useState<SubmissionData[]>()
   const [testCases, setTestCases] = useState<number[][]>([])
   const [submissionIds, setSubmissionIds] = useState<string[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const verdictProperties = Object.keys(emptySubmissionData)
 
-  const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams()
   const problemID = searchParams.get('problem')
 
   const problemsObj = useProblemsObject()
@@ -77,11 +85,14 @@ export default function SubmissionsList() {
         .then((data) => {
           const problemSubmissions = data.submissionsPerProblem[0].submissions
 
-          const buildTable : SubmissionData[] = []
+          const buildTable: SubmissionData[] = []
           const buildTestCases: number[][] = []
           const buildSubmissionIds: string[] = []
           for (const submission of problemSubmissions) {
-            const submissionData : SubmissionData = formatSubmissionData(submission, problemsObj)
+            const submissionData: SubmissionData = formatSubmissionData(
+              submission,
+              problemsObj,
+            )
             buildTable.push(submissionData)
             buildTestCases.push(submission.verdict_list)
             buildSubmissionIds.push(submission.submission_id)
@@ -103,7 +114,7 @@ export default function SubmissionsList() {
   }
 
   if (isLoading) {
-    return <React.Fragment/>
+    return <React.Fragment />
   }
 
   return (
@@ -138,19 +149,27 @@ export default function SubmissionsList() {
             ))}
           </Grid>
         </Box>
-        {
-          submissionsTable?.map((submission, i) => {
-            return (
-            <Button sx = {{width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', borderTop : i > 0 ? 'solid black 1px' : '0px', borderRadius: 0}} 
-            onClick = {() => onSubmissionClick(submissionIds[i])}>
-              <Grid container >
+        {submissionsTable?.map((submission, i) => {
+          return (
+            <Button
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                borderTop: i > 0 ? 'solid black 1px' : '0px',
+                borderRadius: 0,
+              }}
+              onClick={() => onSubmissionClick(submissionIds[i])}
+            >
+              <Grid container>
                 {verdictProperties.map((property, j) => (
                   <Grid
                     item
                     xs={columnWidths[j]}
                     p={1}
                     textAlign={'center'}
-                    pt = {1}
+                    pt={1}
                   >
                     {property === 'date' ? (
                       <Box>
@@ -186,9 +205,8 @@ export default function SubmissionsList() {
                 })}
               </Box>
             </Button>
-            )
-        })
-        }
+          )
+        })}
       </Box>
     </Container>
   )
