9a7114fb637b7f5fc371863463b79e000b792351
sd0623 <deegutlasathwika@gmail.com>
2024-04-03 20:06:01 -0400

    spec page

--- src/pages/About/SpecPage.tsx
+++ src/pages/About/SpecPage.tsx
@@ -1,5 +1,46 @@
 import React from 'react'
 import { Container, Typography } from '@mui/material'
+import MDX from '../../components/MDXRenderer'
+
+const C_notes = `
+## C
+
+#### Compiler Flags
+\`-g -O2 -std=c11\`
+
+#### File Extensions
+Allowed: \`.c\`
+`
+
+const Cpp_notes = `
+## C++
+
+#### Compiler Flags
+\`-g -O2 -std=c++17\`
+
+#### File Extensions
+Allowed: \`.cpp, .cxx, .cc\`
+`
+
+const Java_notes = `
+## Java
+
+#### Args
+\`-Xss64m -Xmx2048m\`
+
+#### File Extensions
+Allowed: \`.java\`
+`
+
+const Py_notes = `
+## Python
+
+#### Compiler
+\`python3\`
+
+#### File Extensions
+Allowed: \`.py\`
+`
 
 export default function SpecPage() {
   return (
@@ -8,10 +49,26 @@ export default function SpecPage() {
         Technical Specifications
       </Typography>
       <Typography variant="body1" paragraph>
-        Languages supported and code samples in C++.
+        Supported languages for submissions are{' '}
+        <strong>C, C++, Java, and Python.</strong>
       </Typography>
       <Typography variant="h3" gutterBottom color="primary">
-        Team
+        Compiler Settings
+      </Typography>
+      <Typography
+        className="markdown themeborder"
+        gutterBottom
+        color="primary"
+        component="span"
+      >
+        <MDX value={C_notes} />
+        <hr />
+        <MDX value={Cpp_notes} />
+        <hr />
+        <MDX value={Java_notes} />
+        <hr />
+        <MDX value={Py_notes} />
+        <hr />
       </Typography>
     </Container>
   )
